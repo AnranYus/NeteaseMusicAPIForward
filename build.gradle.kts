03ac8a90fd215a0bf1765f6e927a451413c26649
Anran <anranyus@hotmail.com>
2023-03-10 22:23:13 +0800

    Add project depend

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,21 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id ("java-library")
+    kotlin("jvm") version "1.7.20"
 }
+
 if(allprojects.isEmpty()){
     repositories {
         mavenCentral()
     }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.9")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 java {
@@ -12,8 +23,12 @@ java {
     targetCompatibility = JavaVersion.VERSION_1_7
 }
 
-dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.9")
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
